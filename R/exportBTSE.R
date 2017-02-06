302661f0bfde7edfec2985f9e45251c9c02bb015
Vince Carey <stvjc@channing.harvard.edu>
2017-02-06 14:49:41 -0500

    moving along

---
+++ R/exportBTSE.R
@@ -0,0 +1,37 @@
+#' @import SummarizedExperiment
+#' @import bigrquery
+#' support for BigTable-backed SummarizedExperiment
+#' @param x \code{\link[SummarizedExperiment]{SummarizedExperiment-class}} instance
+#' @param src src_bigquery instance
+#' @param datasetname identifier for bigtable
+#' @param tablename character string identifying dataset
+#' @param \dots passed down
+#' @aliases "writeBigTable-RangedSummarizedExperiment"
+#' @export
+setGeneric("writeBigTable", function(x, src, featureKeyName, sampleKeyName, tablename, ...)
+   standardGeneric("writeBigTable"))
+setOldClass("src_bigquery")
+setMethod("writeBigTable", c("RangedSummarizedExperiment", "src_bigquery"), function(x, src, featureKeyName, sampleKeyName, tablename, ...) {
+#
+# do we really need two tables?  could join them and just keep field
+# names "separate"
+#
+   dfassay = as.data.frame(assays(x)[[1]])
+   featureKey = rownames(x)
+   if (is.null(featureKey)) stop("need rownames for this operation")
+   dfassay = data.frame(featureKey, dfassay)
+   names(dfassay)[1] = featureKeyName
+   dfmeta = as.data.frame(rowRanges(x))
+   dfmeta = data.frame(featureKey, dfmeta)
+   names(dfmeta)[1] = featureKeyName
+   sampdata = as.data.frame(colData(x))
+   sampdata = data.frame(colnames(x), sampdata)
+   names(sampdata)[1] = sampleKeyName
+   mycon = src$con
+   assaycheck = dbWriteTable(mycon, paste0(tablename, "_assay"), dfassay)
+   rangecheck = dbWriteTable(mycon, paste0(tablename, "_ranges"), dfmeta)
+   sampcheck = dbWriteTable(mycon, paste0(tablename, "_coldata"), sampdata)
+   if (all(c(assaycheck, rangecheck, sampcheck))) return(src)
+   else stop("a write operation failed")
+})
+
