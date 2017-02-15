0385f1109cb44d78467e8715729daa8a2ac9f9ed
shwetagopaul <mail@shwetagopaul92.com>
2017-02-15 15:00:59 -0500

    change to gobr

--- R/gobr.R
+++ R/gobr.R
@@ -7,41 +7,44 @@
 #' @import AnnotationDbi
 #' @param src instance of src_organism in Organism.dplyr
 #' @export
-gobrowse <- function(src) {
+gobrowse = function (src, ont = "BP", BPini = "A-L") 
+{
   require(GO.db)
   require(Organism.dplyr)
   gok = keys(GO.db)
-  godf = AnnotationDbi::select(GO.db, keys=gok, columns=c("GOID", "ONTOLOGY", "TERM"))
-  godf = godf[order(godf$TERM),]
-
-  ui <- miniPage(
-    gadgetTitleBar("Search for a GO category:"),
-    miniContentPanel(
-      selectInput("gosel", "GO:", godf$TERM[1:10000], selected=godf$TERM[1]),
-      dataTableOutput("tab")
-    )
-  )
-
+  godf = AnnotationDbi::select(GO.db, keys = gok, columns = c("GOID", 
+                                                              "ONTOLOGY", "TERM"))
+  godf = godf[which(godf$ONTOLOGY == ont), ]
+  if (ont == "BP") {
+    tag = substr(tolower(godf$TERM), 1, 1)
+    kp = which(tag %in% letters[1:12])
+    if (BPini != "A-L") 
+      kp = -kp
+    godf = godf[kp, ]
+  }
+  godf = godf[order(godf$TERM), ]
+  ui <- miniPage(gadgetTitleBar("Search for a GO category:"), 
+                 miniContentPanel(selectInput("gosel", "GO:", godf$TERM, 
+                                              selected = godf$TERM[1]), dataTableOutput("tab")))
   server <- function(input, output, session) {
-
-    # Render the plot
     output$tab <- renderDataTable({
-      # Plot the data with x/y vars indicated by the caller.
-      ans = godf[ godf$TERM == input$gosel, ]
+      ans = godf[godf$TERM == input$gosel, ]
       curid = ans$GOID
-      genes = (src %>% tbl("id_go") %>% filter(go==curid)) %>% as.data.frame()
-      genes
+      genes = (src %>% tbl("id_go") %>% filter(go == curid)) %>% 
+        as.data.frame()
+      jo = inner_join(tbl(src, "id_go"), tbl(src, "ranges_gene"))
+      res = jo %>% filter(go==curid) %>% collect() %>% GenomicRanges::GRanges()
+      
     })
-
-    # Handle the Done button being pressed.
     observeEvent(input$done, {
-      ans = godf[ godf$TERM == input$gosel, ]
+      ans = godf[godf$TERM == input$gosel, ]
       curid = ans$GOID
-      genes = (src %>% tbl("id_go") %>% filter(go==curid)) %>% as.data.frame()
-      genes
-      stopApp(genes)
+      genes = (src %>% tbl("id_go") %>% filter(go == curid)) %>% 
+        as.data.frame()
+      jo = inner_join(tbl(src, "id_go"), tbl(src, "ranges_gene"))
+      res = jo %>% filter(go==curid) %>% collect() %>% GenomicRanges::GRanges()
+      stopApp(res)
     })
   }
-
   runGadget(ui, server)
 }
