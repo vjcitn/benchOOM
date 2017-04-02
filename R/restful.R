386b90a059a4d244dedad70fd8526a1de020db2f
Vince Carey <stvjc@channing.harvard.edu>
2017-04-02 18:56:35 -0400

    restful interface for HDF5 server draft in

---
+++ R/restful.R
@@ -0,0 +1,95 @@
+#library(rjson)
+#library(httr)
+
+dataref = function(serverURL, host) {
+paste0(serverURL, "/?host=", host)
+}
+
+setClass("H5ShContent", representation(
+# "host" content
+   serverURL="character",
+   host="character",
+   hrefs="character",
+   lastModified="character", created="character",
+   root = "character"))
+setClass("H5SDatasets", representation(dsuuid="character", attrs="list"), contains="H5ShContent")
+setMethod("show", "H5ShContent", function(object) {
+ self = object@hrefs["self"]
+ cat("H5serv content for 'host'", object@host, "\n")
+ cat(" at server", object@serverURL, "\n")
+# cat(" use datasets() ...\n")
+})
+setMethod("show", "H5SDatasets", function(object) {
+ cat("H5SDatasets, (1 of ", length(object@dsuuid), "): ", object@dsuuid[1], "\n", sep="")
+ cat("derived from :\n")
+ callNextMethod()
+})
+
+#' extract dataset UUID and attributes from H5ShContent
+#' @param object instance of H5ShContent
+#' @param \dots not used yet
+#' @export
+#' @exportMethod datasets
+setGeneric("datasets", function(object, ...) standardGeneric("datasets"))
+setMethod("datasets", "H5ShContent", function(object,...) {
+ target = paste0(.serverURL(object), "/datasets?host=", .host(object))
+ lis = fromJSON(readBin(GET(target)$content, w="character"))
+ ds = new("H5SDatasets", object, dsuuid=lis$datasets)
+ atts = dsAttrs(ds)
+ ds@attrs=atts@attrs
+ ds
+})
+# should write (unexported?) methods here
+.serverURL = function(x) x@serverURL
+.host = function(x) x@host
+.root = function(x) x@root
+.dsuuid = function(x, which=1) x@dsuuid[which]
+setGeneric("dsAttrs", function(x, ...) standardGeneric("dsAttrs"))
+setClass("H5DatasetAttributes", representation(attrs="list"), contains="H5SDatasets")
+setMethod("show", "H5DatasetAttributes", function(object) {
+  cat(class(object), "instance\n")
+  callNextMethod()
+})
+setMethod("dsAttrs", "H5SDatasets", function(x, which=1, ...) {
+  target = paste0(.serverURL(x), "/datasets/", .dsuuid(x, which), 
+      "/attr2?host=", .host(x))
+  acon = GET(target)
+  ans = fromJSON(readBin(acon$content, what="character"))
+  new("H5DatasetAttributes", attrs=ans, x)
+})
+  
+
+#' acquire a list of REST content for an HDF5 server query, wrap in H5ShContent ('host' content)
+#' @import httr
+#' @import rjson
+#' @param serverURL a character string giving the URL including port number on which HDF5 server is listening
+#' @param host character
+#' @examples
+#' # this is data served by HDF group
+#' tall = getH5ShContent( serverURL = "https://data.hdfgroup.org:7258", host="tall.data.hdfgroup.org")
+#' tall
+#' tds = datasets(tall)
+#' tds
+#' tds["0:3", "0:3"]
+#' @export
+getH5ShContent = function(serverURL, host) {
+ stopifnot(length(serverURL)==1, length(host)==1)
+ stopifnot(is(c(serverURL, host), "character"))
+ basic = GET( dataref( serverURL, host ) )
+ ans = fromJSON( readBin( basic$content, what = "character" ) )
+ h = ans$hrefs
+ nms = sapply(h, function(x) x[[2]])
+ hr = sapply(h, function(x) x[[1]])
+ names(hr) = nms
+ new("H5ShContent", serverURL=serverURL, host=host, 
+         hrefs=hr, lastModified=ans$lastModified,
+         created=ans$created, root=ans$root)
+}
+
+setMethod("[", c("H5SDatasets", "character", "character"), function (x, i, j, ..., drop = TRUE) 
+ {
+ target = paste0(.serverURL(x), "/datasets/", .dsuuid(x), "/value?host=", .host(x), "&select=[", i, ",", j, "]")
+ val = GET(target)
+ ans = fromJSON( readBin( val$content, what="character" ) )
+ t(do.call(rbind, ans$value))
+ })
